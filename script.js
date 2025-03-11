b7411f9ff491ffaf60caabf32b4ec5f566522553
Adrian T <adrian.toscanoang@gmail.com>
2025-03-10 21:56:03 -0700

    added default piano image

--- script.js
+++ script.js
@@ -12,14 +12,21 @@ function generateRandomImage() {
     // do this while our updated image source is the same name as the current image source name
     do {
 
-        // get image source 
-        // CURRENT ISSUE : WE DONT HAVE A DEFAULT IMAGE. THE IMAGE IS DISPLAYED AFTER THE GENERATE IMAGE BUTTON IS CLICKED. SO I CAN'T GRAB THE CURRENT IMAGE SOURCE ON THE FIRST PASS. I NEED TO DEFAULT A RANDOM IMAGE TO BEGIN WITH. AND THEN EVERY TIME THE BUTTON IS CLICKED, I CAN CHECK AGAINST THAT. THIS IS DONE SO THAT THE USER DOESNT GET THE SAME CHORD OVER AND OVER (BY CHANCE. IT IS NOT AS LIKELY AS THE LIST OF CHORDS GROWS. BUT ITS STILL POSSIBLE AND CAN BE INTERPRETED AS A BUG BY THE USER IF , FOR SOME REASON THEY GENERATE NEW IMAGE AND NOTHING CHANGES.)
-        const currImageSource = document.getElementById()
+        // get current image source 
+        const currImageSource = document.getElementById('randomImage').src
 
-    } while ()
+        // get new image 
+        const randomIndex = Math.floor(Math.random() * images.length);
+        const newImageSource = images[randomIndex];
 
+    } while (currImageSource === newImageSource);
 
+    // assign the newImageSource 
+    document.getElementById('randomImage').src = newImageSource;
+
+    /*
     const randomIndex = Math.floor(Math.random() * images.length); // Select a random index
     const randomImage = images[randomIndex]; // Get the random image from the array
-    document.getElementById('randomImage').src = randomImage; // Update the image src
+    document.getElementById('randomImage').src = randomImage; // Update the image src 
+    */
 }
